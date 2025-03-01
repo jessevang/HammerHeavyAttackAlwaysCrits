56c9b89a5e2a1e438840aeb988c5ac9e2ee0b531
Jesse Vang <jessevang@sbcglobal.net>
2025-03-01 12:42:52 -0800

    Add project files.

---
+++ ModEntry.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using Spacechase.Shared.Patching;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Buffs;
+
+namespace HammerHeavyAttackAlwaysCrits
+{
+    public class Config
+    {
+        public string Note { get; set; } = "additional crit multiplier set below will only apply to the special attack of hammer/clubs";
+        public float ApplyAdditionalCritMultiplierDamage { get; set; } = 0.0f;
+
+    }
+
+    internal sealed class ModEntry : Mod
+    {
+        public static ModEntry Instance { get; private set; }
+        public Config Config { get; private set; }
+
+        
+        public override void Entry(IModHelper helper)
+        {
+            Instance = this; 
+            Config = helper.ReadConfig<Config>() ?? new Config(); 
+
+            //S
+            HarmonyPatcher.Apply(this,
+            new PatchName(Config, Instance)
+            );
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ Patches/PatchName.cs
@@ -0,0 +1,95 @@
+﻿
+using StardewModdingAPI;
+using StardewValley;
+using HarmonyLib;
+using Spacechase.Shared.Patching;
+using StardewValley.Menus;
+using HammerHeavyAttackAlwaysCrits;
+using StardewValley.Tools;
+using Microsoft.Xna.Framework;
+
+/// <summary>Applies prefix Harmony patches to <see cref="MeleeWeapon.triggerClubFunction"/> to add crit damage multiplier to the club special attack.</summary>
+
+internal class PatchName : BasePatcher
+{
+
+    public readonly Config _config;
+    public static ModEntry Instance;
+
+    public PatchName(Config Config)
+    {
+        _config = Config;
+    }
+
+
+    public PatchName(Config Config, ModEntry _instance)
+    {
+        _config = Config;
+        Instance = _instance;
+    }
+
+
+
+
+    public override void Apply(Harmony harmony, IMonitor monitor)
+    {
+        //Applies prefix on MeleeWeapon.triggerClubFunction
+        harmony.Patch(
+            original: this.RequireMethod<MeleeWeapon>(
+            nameof(MeleeWeapon.triggerClubFunction)
+        ), 
+        prefix: this.GetHarmonyMethod(nameof(ModifyTriggerClubFunction)) 
+        );
+
+
+    }
+
+
+
+
+    public static bool ModifyTriggerClubFunction(Game1 __instance, Farmer who)
+    {
+        if (who.CurrentTool.PlayUseSounds)
+        {
+            who.playNearbySoundAll("clubSmash", null);
+        }
+
+        int minDamage = 0;
+        int maxDamage = 0;
+        if(MeleeWeapon.TryGetData(who.CurrentTool.itemId.ToString(), out var weaponData))
+        {
+            minDamage = (int)(weaponData.MinDamage *(weaponData.CritMultiplier + Instance.Config.ApplyAdditionalCritMultiplierDamage));
+            maxDamage = (int)(weaponData.MaxDamage *(weaponData.CritMultiplier + Instance.Config.ApplyAdditionalCritMultiplierDamage));
+        }
+
+        who.currentLocation.damageMonster(new Rectangle((int)who.Position.X - 192, who.GetBoundingBox().Y - 192, 384, 384), minDamage, maxDamage, isBomb: false, 1.5f, 100, 0f, 1f, triggerMonsterInvincibleTimer: false, who);
+        Game1.viewport.Y -= 21;
+        Game1.viewport.X += Game1.random.Next(-32, 32);
+        Vector2 v = who.getUniformPositionAwayFromBox(who.FacingDirection, 64);
+        switch (who.FacingDirection)
+        {
+            case 0:
+            case 2:
+                v.X -= 32f;
+                v.Y -= 32f;
+                break;
+            case 1:
+                v.X -= 42f;
+                v.Y -= 32f;
+                break;
+            case 3:
+                v.Y -= 32f;
+                break;
+        }
+
+        //need to apply a reflection patch to multiplier since it's a protected class
+        //Game1.multiplayer.broadcastSprites(who.currentLocation, new TemporaryAnimatedSprite("TileSheets\\animations", new Rectangle(0, 128, 64, 64), 40f, 4, 0, v, flicker: false, who.FacingDirection == 1));
+        who.jitterStrength = 2f;
+        return false;
+
+    }
+           
+
+
+
+}
